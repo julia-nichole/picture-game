7fe30fa7e5179a756a9fd70b73f629f4537ef19f
julia-nichole <juliagalan191994@icloud.com>
2019-10-30 15:25:32 -0500

    grid

--- js/main.js
+++ js/main.js
@@ -1,10 +1,19 @@
 const pictures = {
-    1:"",
-    2:"",
-    3:"",
-    4:"",
-    5:"",
-    6:""
+    1:"Screen Shot 2019-10-30 at 2.46.18 PM.png",
+    2:"Screen Shot 2019-10-30 at 2.48.00 PM.png",
+    3:"Screen Shot 2019-10-30 at 2.49.13 PM.png",
+    4:"Screen Shot 2019-10-30 at 2.49.43 PM.png",
+    5:"Screen Shot 2019-10-30 at 2.50.35 PM.png",
+    6:"Screen Shot 2019-10-30 at 2.50.57 PM.png"
 
 
 };
+
+
+
+let picArray = [1,2,3,4,5,6];
+grid = [
+    1,2,
+    3,4,
+    4,6
+];
