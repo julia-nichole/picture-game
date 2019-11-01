518622b23c0d5d5c62a1cf1b0654dbc75737bffe
julia-nichole <juliagalan191994@icloud.com>
2019-11-01 14:04:28 -0500

    pictures

--- js/main.js
+++ js/main.js
@@ -1,43 +1,76 @@
+const array = [1, 2, 3, 4];
+
 const coco = {
-    0:{imgUrl:"https://i.imgur.com/J7Iw3J0.jpg"},
-    1:{imgUrl:"https://i.imgur.com/zCe0nHA.jpg"},
-    2:{imgUrl:"https://i.imgur.com/3utOB37.jpg"},
-}
-const arry = [0,1,2]
-
-
-//event listeners//
-const scoreInput = document.querySelector('h2');
-const slotBox = document.querySelector("section");
-const spinner = document.querySelector("button");
-
-function shuffle(arry){
-    if (gameScore === "Winner!") return;
-    var score = "";
-    for (var i = 0; i <arry.length; i++) {
-        score += "" + `<img src="${coco[arry[spinSlot()]].imgUrl}">`;
+    1: {
+        imgUrl: 'https://i.imgur.com/J7Iw3J0.jpg'
+    },
+    2: {
+        imgUrl: 'https://i.imgur.com/zCe0nHA.jpg'
+    },
+    3: {
+        imgUrl: 'https://i.imgur.com/3utOB37.jpg'
+    },
+    4: {
+        imgUrl: 'https://i.imgur.com/44fW61J.jpg'
     }
-    document.getElementById("box").innerHTML = score;
-}
+
+};
+
+
+var gameScore = 0;
+let scores, winner;
+document.getElementById("reset").addEventListener("click", resetGame);
+const scoreInput = document.querySelector('h2');
+const slotBox = document.querySelector(' div');
+const spinner = document.querySelector("button").addEventListener('click', handleClick);
+
+
+
+function shuffle(array) {
+    if (gameScore === "Winner!") return;
+    var x = "";
+    for (var i = 0; i < array.length; i++) {
+        x += "" + `<img src="${coco[array[spinSlot()]].imgUrl}">`;
+    }
+    document.getElementById("slot").innerHTML = x;
+};
+
 function spinSlot() {
     if (gameScore === "Winner!") return;
-    return Math.floor(Math.random() * arry.length)
-}; console.log("YOOOO");
+    return Math.floor(Math.random() * array.length)
+};
 
 
-
-function scoreBoard(){
-    if(("https://i.imgur.com/J7Iw3J0.jpg" === "https://i.imgur.com/J7Iw3J0.jpg")
-    ||("https://i.imgur.com/zCe0nHA.jpg" === "https://i.imgur.com/zCe0nHA.jpg")
-    ||( "https://i.imgur.com/3utOB37.jpg" === "https://i.imgur.com/3utOB37.jpg")){
-       return gameScore = (gameScore + 100);
-} else if ("https://i.imgur.com/J7Iw3J0.jpg" === "https://i.imgur.com/J7Iw3J0.jpg" && "https://i.imgur.com/J7Iw3J0.jpg"
-    ||"https://i.imgur.com/zCe0nHA.jpg" === "https://i.imgur.com/zCe0nHA.jpg" && "https://i.imgur.com/zCe0nHA.jpg"
-    ||"https://i.imgur.com/3utOB37.jpg"=== "https://i.imgur.com/3utOB37.jpg" && "https://i.imgur.com/3utOB37.jpg"){
-        gameScore = (gameScore + 200);
-    } 
-    else if (time === 0 && score < 600) {
-        return "Better luck next time !";
-    }
-        return gameScore;
+function handleClick(evt) {
+    if (gameScore === "Winner!") return;
+    scoreBoard()
+    coins()
+    render();
 }
+
+
+
+function scoreBoard() {
+    if (('https://i.imgur.com/J7Iw3J0.jpg' === 'https://i.imgur.com/J7Iw3J0.jpg') || ('' === '') || ('https://i.imgur.com/3utOB37.jpg' === 'https://i.imgur.com/3utOB37.jpg')) {
+        return gameScore = (gameScore + 100);
+    } else if ('https://i.imgur.com/J7Iw3J0.jpg' === 'https://i.imgur.com/J7Iw3J0.jpg' && 'https://i.imgur.com/J7Iw3J0.jpg' || '' === '' && '' || 'https://i.imgur.com/3utOB37.jpg' === 'https://i.imgur.com/3utOB37.jpg' && 'https://i.imgur.com/3utOB37.jpg') {
+        gameScore = gameScore + 200;
+    }
+    
+    return gameScore;
+
+}
+
+function coins() {
+    if (gameScore >= 1000) { gameScore = "Winner!" };
+}
+
+function render() {
+   
+    scoreInput.textContent = gameScore
+
+}
+
+function resetGame(evt) {
+    window.location.reload();
+  }
